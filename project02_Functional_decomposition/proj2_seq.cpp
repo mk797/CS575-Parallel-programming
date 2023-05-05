b6ddb391fa69b879ccb02f707007abf7b3db5768
mk797 <55266144+mk797@users.noreply.github.com>
2023-05-05 15:19:28 -0700

    Update proj2_seq.cpp
    changed order for watcher

--- project02_Functional_decomposition/proj2_seq.cpp
+++ project02_Functional_decomposition/proj2_seq.cpp
@@ -37,6 +37,9 @@ const float MIDPRECIP =             14.0;
 
 unsigned int seed = 0;
 
+float rand1;
+float rand2;
+
 float Ranf( unsigned int *seedp,  float low, float high )
 {
         float r = (float) rand_r( seedp );              // 0 - RAND_MAX
@@ -68,37 +71,12 @@ NowNumWolves = 1;
     while(NowYear < 2029)
     {
 
-    
-// watcher first
-
-    float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
-    float temp = AVG_TEMP - AMP_TEMP * cos( ang );
-   
-    float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
-   
-    if( NowPrecip < 0. )
-        NowPrecip = 0.;
-    tempFactor = exp(   -Sqr(  ( NowTemp - MIDTEMP ) / 10.  )   );
-    precipFactor = exp(   -Sqr(  ( NowPrecip - MIDPRECIP ) / 10.  )   );
-
-
-
-
-
-
-
-
 //grass first
     nextHeight = NowHeight;
     nextHeight += tempFactor * precipFactor * RYEGRASS_GROWS_PER_MONTH;
     nextHeight -= (float)NowNumRabbits * ONE_RABBITS_EATS_PER_MONTH;
  
     if( nextHeight < 0. ) nextHeight = 0.;
-
-    
-
-   
-
 
 
     //rabbit first
@@ -135,10 +113,6 @@ NowNumWolves = 1;
         }
     
 
-    // watcher second
-
-    NowTemp = temp + Ranf( &seed, -RANDOM_TEMP, RANDOM_TEMP );
-    NowPrecip = precip + Ranf( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
 
     // grass second
 
@@ -151,8 +125,27 @@ NowNumWolves = 1;
      NowNumWolves = NextNumWolves;
 
   
- fprintf(stderr, "%d,%d,%lf,%lf,%lf\n", NowNumRabbits,NowNumWolves, 2.54*NowHeight,NowPrecip,((5./9.)*(NowTemp-32))  );
+    fprintf(stderr, "%lf,%lf,%d,%d,%lf,%lf,%lf\n",rand1,rand2, NowNumRabbits,NowNumWolves, 2.54*NowHeight,NowPrecip,((5./9.)*(NowTemp-32))  );
  
+    
+// watcher first
+
+    float ang = (  30.*(float)NowMonth + 15.  ) * ( M_PI / 180. );
+    float temp = AVG_TEMP - AMP_TEMP * cos( ang );
+   
+    float precip = AVG_PRECIP_PER_MONTH + AMP_PRECIP_PER_MONTH * sin( ang );
+   
+    if( NowPrecip < 0. )
+        NowPrecip = 0.;
+    tempFactor = exp(   -Sqr(  ( NowTemp - MIDTEMP ) / 10.  )   );
+    precipFactor = exp(   -Sqr(  ( NowPrecip - MIDPRECIP ) / 10.  )   );
+
+    // watcher second
+
+   
+    NowTemp = temp + Ranf( &seed, -RANDOM_TEMP, RANDOM_TEMP );
+    NowPrecip = precip + Ranf( &seed,  -RANDOM_PRECIP, RANDOM_PRECIP );
+
       NowMonth++;
     if(NowMonth==12)
     {
