f0cad86acaacecc713dedb60aee1efb98d03c3ed
mk797 <55266144+mk797@users.noreply.github.com>
2023-05-17 21:47:44 -0700

    Update project04.bash
    bash

--- project04/project04.bash
+++ project04/project04.bash
@@ -1,7 +1,7 @@
 
 	printf "Arraysize,Number if threads,performanceMult,Performance_MultSUm\n";
 
-for t in 1 2 4 8 16 32
+for t in 1 2 4 
 do
     for n in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
     do
