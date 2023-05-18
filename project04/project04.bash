5d4ccd9ef02353fd9b42729528b6a5b44b6e0386
mk797 <55266144+mk797@users.noreply.github.com>
2023-05-17 20:23:03 -0700

    Update project04.bash
    numt bash

--- project04/project04.bash
+++ project04/project04.bash
@@ -2,9 +2,12 @@
 
 	printf "Arraysize,NonsimdMul,SimdMul,performanceMul,NonSimdmulSum,SimdmulSum,Performance_MulSUm\n";
 
-for n in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2090152 4194304 8388608
+for n in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
 do
-    g++   project04.cpp  -DARRAYSIZE=$n  -o proj04  -lm  -fopenmp
+  for t in 4 8 16 32 64
+    do
+    g++   project04.cpp  -DARRAYSIZE=$n -DNUMT=$t  -o proj04  -lm  -fopenmp
     ./proj04
+    done
 done
 
