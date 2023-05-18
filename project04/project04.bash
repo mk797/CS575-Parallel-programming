ec48381f4c7c38a950c3c6b06ce751e88652b94f
Ranganath Manikanta <manikan@flip3.engr.oregonstate.edu>
2023-05-17 18:00:02 -0700

    proj

---
+++ project04/project04.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for n in 1024 2048 302 4096 8192 16384 32768 65536131072 262144524288 10485762090152  41943048388608
+do
+    g++   project04.cpp  -DARRAYSIZE=$t  -o proj04  -lm  -fopenmp
+    ./proj04
+done
+
