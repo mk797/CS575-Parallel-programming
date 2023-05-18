6a6c2d89cefca11b81ba75e8d862f09ff791c9b3
mk797 <55266144+mk797@users.noreply.github.com>
2023-05-17 20:52:23 -0700

    Update project04.bash
    update threads

--- project04/project04.bash
+++ project04/project04.bash
@@ -1,13 +1,11 @@
-#!/bin/bash
 
-	printf "Arraysize,NonsimdMul,SimdMul,performanceMul,NonSimdmulSum,SimdmulSum,Performance_MulSUm\n";
+	printf "Arraysize,Number if threads,performanceMult,Performance_MultSUm\n";
 
-for n in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
+for t in 1 2 4 8 16 32
 do
-  for t in 4 8 16 32 64
+    for n in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608
     do
-    g++   project04.cpp  -DARRAYSIZE=$n -DNUMT=$t  -o proj04  -lm  -fopenmp
+    g++   Project04_extra.cpp  -DARRAYSIZE=$n -DNUMT=$t  -o proj04  -lm  -fopenmp
     ./proj04
     done
 done
-
