e570a5f8eea7d11e34988a2902a52e9301adcecf
mk797 <55266144+mk797@users.noreply.github.com>
2023-05-17 18:20:03 -0700

    Update project04.bash
    csv

--- project04/project04.bash
+++ project04/project04.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+	printf "Arraysize,NonsimdMul,SimdMul,performanceMul,NonSimdmulSum,SimdmulSum,Performance_MulSUm\n";
+
 for n in 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2090152 4194304 8388608
 do
     g++   project04.cpp  -DARRAYSIZE=$n  -o proj04  -lm  -fopenmp
